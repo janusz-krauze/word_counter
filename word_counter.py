de2c83628ce570a3dbe5397785903876cab987ac
Krauze <Janusz.Krauze@nielsen.com>
2017-10-30 16:00:51 +0100

    implement functions in word_counter.py

--- word_counter.py
+++ word_counter.py
@@ -1,27 +1,42 @@
 
 import argparse
 
+def count_lines(file_path):
+    data_file = open(file_path, 'r')
+    n_lines = 0
+    for line in data_file:
+        n_lines += 1
+    data_file.close()
+    return(n_lines)
+    
+def count_words(file_path):
+    data_file = open(file_path, 'r')
+    n_words = 0
+    for line in data_file:
+        while "  " in line:
+            line = line.replace("  "," ")
+        words = line.strip().split(" ")
+        if words != ['']:
+            n_words += len(words)
+    data_file.close()
+    return(n_words)
+    
+def count_chars(file_path):
+    data_file = open(file_path, 'r')
+    n_chars = 0
+    for line in data_file:
+        n_chars += len(line) + 1
+    data_file.close()
+    return(n_chars)
+
 parser = argparse.ArgumentParser(description='Count lines, words and characters.')
 parser.add_argument('file_path', type=str, help='name of the file to be counted')
 
 args = parser.parse_args()
 
-n_lines = 0
-n_words = 0
-n_chars = 0
-
-data_file = open(args.file_path, 'r')
-
-for line in data_file:
-    n_chars += len(line) + 1
-    while "  " in line:
-        line = line.replace("  "," ")
-    words = line.strip().split(" ")
-    if words != ['']:
-        n_words += len(words)
-    n_lines += 1
-    
-data_file.close()    
+n_lines = count_lines(args.file_path)
+n_words = count_words(args.file_path)
+n_chars = count_chars(args.file_path)
 
 print("Number of lines:", str(n_lines))
 print("Number of words:", str(n_words))
