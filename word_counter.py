f9426ba78a20a882cd77dac960e234aca3a024da
janusz-krauze <janusz.krauze@nielsen.com>
2017-11-10 16:29:14 +0100

    add "-l -w -c" options

--- word_counter.py
+++ word_counter.py
@@ -1,42 +1,59 @@
 
 import argparse
 
-def count_lines(file_path):
-    data_file = open(file_path, 'r')
-    n_lines = 0
-    for line in data_file:
-        n_lines += 1
-    data_file.close()
+def count_lines(line):
+    n_lines = 1
     return(n_lines)
     
-def count_words(file_path):
-    data_file = open(file_path, 'r')
+def count_words(line):
     n_words = 0
-    for line in data_file:
-        while "  " in line:
-            line = line.replace("  "," ")
-        words = line.strip().split(" ")
-        if words != ['']:
-            n_words += len(words)
-    data_file.close()
+    while "  " in line:
+        line = line.replace("  "," ")
+    words = line.strip().split(" ")
+    if words != ['']:
+        n_words += len(words)
     return(n_words)
     
-def count_chars(file_path):
-    data_file = open(file_path, 'r')
-    n_chars = 0
-    for line in data_file:
-        n_chars += len(line) + 1
-    data_file.close()
+def count_chars(line):
+    n_chars = len(line) + 1
     return(n_chars)
 
-parser = argparse.ArgumentParser(description='Count lines, words and characters.')
-parser.add_argument('file_path', type=str, help='name of the file to be counted')
+def parse_args(params):
+    parser = argparse.ArgumentParser(description='Count lines, words and characters.')
+    parser.add_argument('file_path', type=str, help='name of the file to be counted')
+    parser.add_argument('-l', dest= "lines", action="store_true", help='count lines')
+    parser.add_argument('-c', dest= "characters", action="store_true", help='count chars')
+    parser.add_argument('-w', dest= "words", action="store_true", help='count words')
 
-args = parser.parse_args()
+    args = parser.parse_args(params)
+    return(args)
 
-try:
-    print("Number of lines:", str(count_lines(args.file_path)))
-    print("Number of words:", str(count_words(args.file_path)))
-    print("Number of characters:", str(count_chars(args.file_path)))
-except OSError:
-    print("File not found.")
+def open_file(file_path):
+    try:
+        data_file = open(file_path, 'r')
+        return(True, data_file)
+    except OSError:
+        return(False, 'File not found')
+
+if __name__ == '__main__':
+    args = parse_args('-l -w -c test_text.txt'.split())
+    data_file = open_file(args.file_path)
+    if data_file[0] == True:
+        lines = 0
+        words = 0
+        chars = 0
+        for data_line in data_file[1]:
+            if args.lines:
+                lines += count_lines(data_line)
+            if args.words:
+                words += count_words(data_line)
+            if args.characters:
+                chars += count_chars(data_line)
+        if args.lines:
+            print("Number of lines: " + str(lines))
+        if args.words:
+            print("Number of words: " + str(words))
+        if args.characters:
+            print("Number of characters: " + str(chars))
+    else:
+        print(data_file[1])
